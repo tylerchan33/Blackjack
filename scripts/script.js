eface45a064955b9c41ce3716cd6accb421bbc6a
tylerchan33 <tylerchan33@gmail.com>
2022-08-22 16:33:18 -0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -14,18 +14,272 @@ window.onload = function (e) {
     ctx.drawImage(jigsaw, canvas.width / 2 - 60, 300, 100, 100)
 }   
 
-// const cards = {
-//     twoOfClubs: {
-//         name: "2OfClubs",
-//         src: "../media/2_of_clubs.png",
-//         value: 2,
-//         // console.log(cards["twoOfClubs"]["src"])
-//     }
-//     threeOfClubs: {
-//         name: "2OfClubs"
-//     }
-
-// }
+const cards = {
+    aceOfClubs: {
+        name: "aceOfClubs",
+        src: "../media/ace_of_clubs.png",
+        value: 1,
+    },
+    twoOfClubs: {
+        name: "2OfClubs",
+        src: "../media/2_of_clubs.png",
+        value: 2,
+        // console.log(cards["twoOfClubs"]["src"])
+    },
+    threeOfClubs: {
+        name: "3OfClubs",
+        src: "../media/3_of_clubs.png",
+        value: 3,
+    },
+    fourOfClubs: {
+        name: "4OfClubs",
+        src: "../media/4_of_clubs.png",
+        value: 4,
+    },
+    fiveOfClubs: {
+        name: "5OfClubs",
+        src: "../media/5_of_clubs.png",
+        value: 5,
+    },
+    sixOfClubs: {
+        name: "6OfClubs",
+        src: "../media/6_of_clubs.png",
+        value: 6,
+    },
+    sevenOfClubs: {
+        name: "7OfClubs",
+        src: "../media/7_of_clubs.png",
+        value: 7,
+    },
+    eightOfClubs: {
+        name: "8OfClubs",
+        src: "../media/8_of_clubs.png",
+        value: 8,
+    },
+    nineOfClubs: {
+        name: "9OfClubs",
+        src: "../media/9_of_clubs.png",
+        value: 9,
+    },
+    tenOfClubs: {
+        name: "10OfClubs",
+        src: "../media/9_of_clubs.png",
+        value: 10,
+    },
+    jackOfClubs: {
+        name: "jackOfClubs",
+        src: "../media/jack_of_clubs.png",
+        value: 10,
+    },
+    queenOfClubs: {
+        name: "queenOfClubs",
+        src: "../media/queen_of_clubs.png",
+        value: 10,
+    },
+    kingOfClubs: {
+        name: "kingOfClubs",
+        src: "../media/king_of_clubs.png",
+        value: 10,
+    },
+    aceOfDiamonds: {
+        name: "aceOfDiamonds",
+        src: "../media/ace_of_diamonds.png",
+        value: 1,
+    },
+    twoOfDiamonds: {
+        name: "2OfDiamonds",
+        src: "../media/2_of_diamonds.png",
+        value: 2,
+        
+    },
+    threeOfDiamonds: {
+        name: "3OfDiamonds",
+        src: "../media/3_of_diamonds.png",
+        value: 3,
+    },
+    fourOfDiamonds: {
+        name: "4OfDiamonds",
+        src: "../media/4_of_diamonds.png",
+        value: 4,
+    },
+    fiveOfDiamonds: {
+        name: "5OfDiamonds",
+        src: "../media/5_of_diamonds.png",
+        value: 5,
+    },
+    sixOfDiamonds: {
+        name: "6OfDiamonds",
+        src: "../media/6_of_diamonds.png",
+        value: 6,
+    },
+    sevenOfDiamonds: {
+        name: "7OfDiamonds",
+        src: "../media/7_of_diamonds.png",
+        value: 7,
+    },
+    eightOfDiamonds: {
+        name: "8OfDiamonds",
+        src: "../media/8_of_diamonds.png",
+        value: 8,
+    },
+    nineOfDiamonds: {
+        name: "9OfDiamonds",
+        src: "../media/9_of_diamonds.png",
+        value: 9,
+    },
+    tenOfDiamonds: {
+        name: "10OfDiamonds",
+        src: "../media/9_of_diamonds.png",
+        value: 10,
+    },
+    jackOfDiamonds: {
+        name: "jackOfDiamonds",
+        src: "../media/jack_of_diamonds.png",
+        value: 10,
+    },
+    queenOfDiamonds: {
+        name: "queenOfDiamonds",
+        src: "../media/queen_of_diamonds.png",
+        value: 10,
+    },
+    kingOfDiamonds: {
+        name: "kingOfDiamonds",
+        src: "../media/king_of_diamonds.png",
+        value: 10,
+    }, 
+    aceOfHearts: {
+        name: "aceOfHearts",
+        src: "../media/ace_of_hearts.png",
+        value: 1,
+    },
+    twoOfHearts: {
+        name: "2OfHearts",
+        src: "../media/2_of_hearts.png",
+        value: 2,
+        
+    },
+    threeOfHearts: {
+        name: "3OfHearts",
+        src: "../media/3_of_hearts.png",
+        value: 3,
+    },
+    fourOfHearts: {
+        name: "4OfHearts",
+        src: "../media/4_of_hearts.png",
+        value: 4,
+    },
+    fiveOfHearts: {
+        name: "5OfHearts",
+        src: "../media/5_of_hearts.png",
+        value: 5,
+    },
+    sixOfHearts: {
+        name: "6OfHearts",
+        src: "../media/6_of_hearts.png",
+        value: 6,
+    },
+    sevenOfHearts: {
+        name: "7OfHearts",
+        src: "../media/7_of_hearts.png",
+        value: 7,
+    },
+    eightOfHearts: {
+        name: "8OfHearts",
+        src: "../media/8_of_hearts.png",
+        value: 8,
+    },
+    nineOfHearts: {
+        name: "9OfHearts",
+        src: "../media/9_of_hearts.png",
+        value: 9,
+    },
+    tenOfHearts: {
+        name: "10OfHearts",
+        src: "../media/9_of_hearts.png",
+        value: 10,
+    },
+    jackOfHearts: {
+        name: "jackOfHearts",
+        src: "../media/jack_of_hearts.png",
+        value: 10,
+    },
+    queenOfHearts: {
+        name: "queenOfHearts",
+        src: "../media/queen_of_hearts.png",
+        value: 10,
+    },
+    kingOfHearts: {
+        name: "kingOfHearts",
+        src: "../media/king_of_hearts.png",
+        value: 10,
+    }, 
+    aceOfSpades: {
+        name: "aceOfSpades",
+        src: "../media/ace_of_spades.png",
+        value: 1,
+    },
+    twoOfSpades: {
+        name: "2OfSpades",
+        src: "../media/2_of_spades.png",
+        value: 2,
+        
+    },
+    threeOfSpades: {
+        name: "3OfSpades",
+        src: "../media/3_of_spades.png",
+        value: 3,
+    },
+    fourOfSpades: {
+        name: "4OfSpades",
+        src: "../media/4_of_spades.png",
+        value: 4,
+    },
+    fiveOfSpades: {
+        name: "5OfSpades",
+        src: "../media/5_of_spades.png",
+        value: 5,
+    },
+    sixOfSpades: {
+        name: "6OfSpades",
+        src: "../media/6_of_spades.png",
+        value: 6,
+    },
+    sevenOfSpades: {
+        name: "7OfSpades",
+        src: "../media/7_of_spades.png",
+        value: 7,
+    },
+    eightOfSpades: {
+        name: "8OfSpades",
+        src: "../media/8_of_spades.png",
+        value: 8,
+    },
+    nineOfSpades: {
+        name: "9OfSpades",
+        src: "../media/9_of_spades.png",
+        value: 9,
+    },
+    tenOfSpades: {
+        name: "10OfSpades",
+        src: "../media/9_of_spades.png",
+        value: 10,
+    },
+    jackOfSpades: {
+        name: "jackOfSpades",
+        src: "../media/jack_of_spades.png",
+        value: 10,
+    },
+    queenOfSpades: {
+        name: "queenOfSpades",
+        src: "../media/queen_of_spades.png",
+        value: 10,
+    },
+    kingOfSpades: {
+        name: "kingOfSpades",
+        src: "../media/king_of_spades.png",
+        value: 10,
+    }, 
+}
 let test2 = document.querySelector("#twoOfClubs").value
 let gameInProgress = false
 let gameOver = false
@@ -63,10 +317,7 @@ function deck () {
 deck()
 console.log(deck())
 
-function setValues () {
-    let cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-}
 
 let fullDeck = deck()
 
